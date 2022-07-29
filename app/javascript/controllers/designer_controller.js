b185a52b188c61f1d6a2f0b98c738eaf29005145
Daniel Grieve <dnlgrv@hey.com>
2022-07-29 10:47:59 +0100

    Add canvas editor with text controls and inspector

---
+++ app/javascript/controllers/designer_controller.js
@@ -0,0 +1,44 @@
+import { Controller } from "@hotwired/stimulus"
+import { fabric } from "fabric"
+
+export default class extends Controller {
+  static targets = ["canvas"]
+
+  connect() {
+    this.canvas = new fabric.Canvas(this.canvasTarget, {
+      backgroundColor: "white"
+    })
+
+    this.canvas.on("object:moving", (event) => {
+      this.dispatch("objectUpdated", { detail: { event } })
+    })
+
+    this.canvas.on("object:modified", (event) => {
+      this.dispatch("objectUpdated", { detail: { event } })
+    })
+
+    this.canvas.on("selection:created", (event) => {
+      this.dispatch("objectSelected", { detail: { event } })
+    })
+
+    this.canvas.on("selection:cleared", () => {
+      this.dispatch("selectionCleared", {})
+    })
+
+    this.canvas.setDimensions({
+      height: "100%",
+      width: "100%",
+    }, { cssOnly: true })
+  }
+
+  addTextBlock() {
+    const text = new fabric.Textbox("Double-click to edit me...", {
+      fontSize: 14,
+      left: 20,
+      top: 20,
+      width: 120
+    })
+
+    this.canvas.add(text)
+  }
+}
---
+++ app/javascript/controllers/inspector_controller.js
@@ -0,0 +1,33 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["layout", "layoutX", "layoutY"]
+
+  connect() {
+    // this.hide()
+  }
+
+  show(event) {
+    this.layoutTarget.style.display = "block"
+    this.update(event)
+  }
+
+  hide() {
+    this.layoutTarget.style.display = "none"
+  }
+
+  update(event) {
+    if (event.detail.event.selected && event.detail.event.selected.length == 1) {
+      const selectedObject = event.detail.event.selected[0]
+      this._updateValues(selectedObject)
+    } else if (event.detail.event.target) {
+      const selectedObject = event.detail.event.target
+      this._updateValues(selectedObject)
+    }
+  }
+
+  _updateValues(object) {
+    this.layoutXTarget.value = object.left
+    this.layoutYTarget.value = object.top
+  }
+}
